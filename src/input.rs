6089dd1e98def5f89f7a07861a3471762b258f87
Syniis <fynn.c.lohren@gmail.com>
2023-02-11 01:11:23 +0100

    Small code fix

--- src/input.rs
+++ src/input.rs
@@ -4,7 +4,6 @@ use bevy::prelude::*;
 pub struct CursorPos(pub Vec2);
 
 pub fn update_cursor_pos(
-    windows: Res<Windows>,
     camera_query: Query<(&Camera, &GlobalTransform)>,
     mut cursor_moved_events: EventReader<CursorMoved>,
     mut cursor_pos: ResMut<CursorPos>,
@@ -15,11 +14,12 @@ pub fn update_cursor_pos(
     };
 
     for moved_event in cursor_moved_events.iter() {
-        let new = camera
-            .viewport_to_world(&transform, moved_event.position)
-            .unwrap()
-            .origin
-            .truncate();
+        let Some(new) =
+            camera.viewport_to_world(&transform, moved_event.position)
+            .map(|ray| ray.origin.truncate())
+        else {
+            return;
+        };
         *cursor_pos = CursorPos(new);
     }
 }
